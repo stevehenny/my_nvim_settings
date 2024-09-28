7be972992dc434293e45a216bb6b0858e3a73e55
stevehenny <132288391+stevehenny@users.noreply.github.com>
2024-09-27 20:08:27 -0600

    Update lspconfig.lua

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -5,17 +5,34 @@ local capabilities = config.capabilities
 
 local lspconfig = require("lspconfig")
 
+
 lspconfig.pyright.setup({
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = {"python"},
 })
 
-lspconfig.clangd.setup {
+lspconfig.clangd.setup({
   on_attach = function(client, bufner)
     client.server_capabilities.signatureHelpProvider = false
     on_attach(client, bufner)
   end,
   capabilities = capabilities
-}
+})
+
+lspconfig.asm_lsp.setup({
+  on_attach = function(client, bufner)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufner)
+  end,
+  capabilities = capabilities
+})
+
+lspconfig.verible.setup({
+  on_attach = function(client, bufner)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufner)
+  end,
+  capabilities = capabilities
+})
 

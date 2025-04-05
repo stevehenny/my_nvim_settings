b064fcc263f1f2030bd5e53562ded3119d35f27a
stevehenny <stephen.henstrom@gmail.com>
2025-04-05 03:33:44 -0600

    adding new error checking stuff but need to add more. Going to bed

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,19 +1,19 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- EXAMPLE
 local servers = { "html", "cssls", "clangd" }
-local nvlsp = require "nvchad.configs.lspconfig"
+local nvlsp = require("nvchad.configs.lspconfig")
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = nvlsp.on_attach,
+		on_init = nvlsp.on_init,
+		capabilities = nvlsp.capabilities,
+	})
 end
 
 -- configuring single server, example: typescript
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,70 +1,108 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre",
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre",
+		opts = require("configs.conform"),
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim",
-        "lua",
-        "vimdoc",
-        "html",
-        "css",
-        "python",
-      },
-    },
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"vimdoc",
+				"html",
+				"css",
+				"python",
+			},
+		},
+	},
 
-  -- Mason Plugin for Managing LSP Installations and Tools
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-    opts = {
-      ensure_installed = {
-        "clang-format", -- clang-format tool
-        "debugpy", -- debugpy tool
-      },
-    },
-  },
+	-- Mason Plugin for Managing LSP Installations and Tools
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+		opts = {
+			ensure_installed = {
+				"clang-format", -- clang-format tool
+				"debugpy", -- debugpy tool
+			},
+		},
+	},
 
-  -- Mason LSP Config to Automatically Install LSP Servers
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup {
-        ensure_installed = { "clangd" }, -- Ensure clangd is installed
-      }
-    end,
-  },
+	-- Mason LSP Config to Automatically Install LSP Servers
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "clangd" }, -- Ensure clangd is installed
+			})
+		end,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("lspconfig").clangd.setup {
-        cmd = { "clangd", "--compile-commands-dir=/path/to/your/project" },
-        filetypes = { "cpp", "c", "cuda" },
-        settings = {
-          clangd = {
-            fallbackFlags = {
-              "-std=c++17",
-              "--cuda-host-only",
-            },
-          },
-        },
-      }
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("lspconfig").clangd.setup({
+				cmd = { "clangd", "--compile-commands-dir=/path/to/your/project" },
+				filetypes = { "cpp", "c", "cuda" },
+				settings = {
+					clangd = {
+						fallbackFlags = {
+							"-std=c++17",
+							"--cuda-host-only",
+						},
+					},
+				},
+			})
+		end,
+	},
+
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
 }

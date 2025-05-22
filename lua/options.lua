6c4cd771d8aa2739d9c0c9018ac3950af63a63f5
stevehenny <stephen.henstrom@gmail.com>
2025-05-22 17:29:42 -0600

    new options

--- lua/options.lua
+++ lua/options.lua
@@ -5,8 +5,8 @@ require("nvchad.options")
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
--- vim.o.guicursor = "v-n:block-blinkwait700-blinkoff400-blinkon250,v-c:block,i-ci:block,r:block"
-vim.o.guicursor = "v-n-c-i-ci-r:block-blinkwait700-blinkoff400-blinkon250"
+vim.o.guicursor = "v-n:block-blinkwait700-blinkoff400-blinkon250,v-c:block,i-ci:block,r:block"
+-- vim.o.guicursor = "v-n-c-i-ci-r:block-blinkwait700-blinkoff400-blinkon250"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.api.nvim_set_keymap("n", "<leader>ld", ":m+1<CR>", { noremap = true, silent = true })
